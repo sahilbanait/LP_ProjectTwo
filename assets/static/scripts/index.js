f0de5deeb302c58fd073d6232d00a49576c6d8d6
Sahil Kumar <sahil.saini526@gmail.com>
2022-06-04 20:42:24 +0800

    index.js

--- assets/static/scripts/index.js
+++ assets/static/scripts/index.js
@@ -1,5 +1,5 @@
 let questionTotal = document.getElementById('questions_total');
-let currentQuestion = document.getElementById('current_question');
+const currentQuestionNumber = document.getElementById('current_question');
 let progressBar = document.getElementById('progressBar');
 let progress = document.getElementById('progress');
 let homepageSection = document.getElementById('homepage-section');
@@ -8,114 +8,162 @@ let contentSection = document.getElementById('content-section');
 let submitBtn = document.getElementById('name-button');
 let nameInput = document.querySelector('#name-input').value;
 let errmsg = document.getElementById('error-meesage');
+let choices = Array.from(document.getElementsByClassName('choice-text'));
+let question = document.getElementById('questions');
+console.log(choices);
 
-window.onload = () => {
-    const quizQuestions = [
-        {
-            questionNumber: 1,
-            question: "What type of computing technology refers to services and applications " + "that typically run on a distributed network through virtualized resources?",
-            choices: {1: "Distributed Computing", 2: "Cloud Computing", 3: "Soft Computing", 4: "Parallel Computing"},
-            answer: 2,
-            percentageBar: 10
-
-        }, {
-            questionNumber: 2,
-            question: "Which one of the following options can be considered as the Cloud?",
-            choices: {1: "Hadoop", 2: "Intranet", 3: "Web Applications", 4: "All the mentioned"},
-            answer: 1,
-            percentageBar: 20
-
-        }, {
-            questionNumber: 3,
-            question: "Cloud computing is a kind of abstraction which is based on the notion of combining physical resources and" + " represents them as _______resources to users",
-            choices: {1: "Real", 2: "Cloud", 3: "Virtual", 4: "none of the mentioned"},
-            answer: 3,
-            percentageBar: 30
-
-        }, {
-            questionNumber: 4,
-            question: "Which of the following has many features of that is now known as cloud computing?",
-            choices: {1: "Web Service", 2: "Softwares", 3: "All of the mentioned", 4: "Internet"},
-            answer: 4,
-            percentageBar: 40
-
-        }, {
-            questionNumber: 5,
-            question: "Which one of the following cloud concepts is related to sharing and pooling the resources?",
-            choices: {1: "Polymorphism", 2: "Virtualization", 3: "Abstraction", 4: "None of the mentioned"},
-            answer: 2,
-            percentageBar: 50
-
-        }, {
-            questionNumber: 6,
-            question: "Which one of the following can be considered as a utility is a dream" + " that dates from the beginning of the computing industry itself?",
-            choices: {1: "Computing", 2: "Model", 3: "Software", 4: "All of the mentioned"},
-            answer: 1,
-            percentageBar: 60
-
-        }, {
-            questionNumber: 7,
-            question: "Which of the following is an essential concept related to Cloud?",
-            choices: {1: "Reliability", 2: "Abstraction", 3: "Productivity", 4: "All of the mentioned"},
-            answer: 2,
-            percentageBar: 70
-
-        }, {
-            questionNumber: 8,
-            question: "Which one of the following is Cloud Platform by Amazon",
-            choices: {1: "Azure", 2: "AWS", 3: "Cloudera", 4: "ll of the mentioned"},
-            answer: 2,
-            percentageBar: 80
-
-        }, {
-            questionNumber: 9,
-            question: "Which one of the following a technology works behind the cloud computing platform?",
-            choices: {1: "Virtualization", 2: "SOA", 3: "Grid Computing", 4: "All of the above"},
-            answer: 4,
-            percentageBar: 90
+let currentQuestion = {};
+let acceptingAnswer = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestion = [];
 
 
-        }, {
-            questionNumber: 10,
-            question: "Which of the following is a characteristic of the SaaS applications?",
-            choices: {
-                1: "SaaS applications are reliable",
-                2: "SaaS applications are not customizable",
-                3: "SaaS applications are customizable",
-                4: "Non-reliable"
-            },
-            answer: 3,
-            percentageBar: 10
+let quizQuestions = [
+    {
+        questionNumber: 1,
+        question: "What type of computing technology refers to services and applications " + "that typically run on a distributed network through virtualized resources?",
+        choice1: "Distributed Computing",
+        choice2: "Cloud Computing",
+        choice3: "Soft Computing",
+        choice4: "Parallel Computing",
+        answer: 2,
+        percentageBar: 10
+
+    }, {
+        questionNumber: 2,
+        question: "Which one of the following options can be considered as the Cloud?",
+        choice1: "Hadoop",
+        choice2: "Intranet",
+        choice3: "Web Applications",
+        choice4: "All the mentioned",
+        answer: 1,
+        percentageBar: 20
+
+    }, {
+        questionNumber: 3,
+        question: "Cloud computing is a kind of abstraction which is based on the notion of combining physical resources and" + " represents them as _______resources to users",
+        choice1: "Real",
+        choice2: "Cloud",
+        choice3: "Virtual",
+        choice4: "none of the mentioned",
+        answer: 3,
+        percentageBar: 30
+
+    }, {
+        questionNumber: 4,
+        question: "Which of the following has many features of that is now known as cloud computing?",
+        choice1: "Web Service",
+        choice2: "Softwares",
+        choice3: "All of the mentioned",
+        choice4: "Internet",
+        answer: 4,
+        percentageBar: 40
+
+    }, {
+        questionNumber: 5,
+        question: "Which one of the following cloud concepts is related to sharing and pooling the resources?",
+        choice1: "Polymorphism",
+        choice2: "Virtualization",
+        choice3: "Abstraction",
+        choice4: "None of the mentioned",
+        answer: 2,
+        percentageBar: 50
+
+    }, {
+        questionNumber: 6,
+        question: "Which one of the following can be considered as a utility is a dream" + " that dates from the beginning of the computing industry itself?",
+        choice1: "Computing",
+        choice2: "Model",
+        choice3: "Software",
+        choice4: "All of the mentioned",
+        answer: 1,
+        percentageBar: 60
+
+    }, {
+        questionNumber: 7,
+        question: "Which of the following is an essential concept related to Cloud?",
+        choice1: "Reliability",
+        choice2: "Abstraction",
+        choice3: "Productivity",
+        choice4: "All of the mentioned",
+        answer: 2,
+        percentageBar: 70
+
+    }, {
+        questionNumber: 8,
+        question: "Which one of the following is Cloud Platform by Amazon",
+        choice1: "Azure",
+        choice2: "AWS",
+        choice3: "Cloudera",
+        choice4: "ll of the mentioned",
+        answer: 2,
+        percentageBar: 80
+
+    }, {
+        questionNumber: 9,
+        question: "Which one of the following a technology works behind the cloud computing platform?",
+        choice1: "Virtualization",
+        choice2: "SOA",
+        choice3: "Grid Computing",
+        choice4: "All of the above",
+        answer: 4,
+        percentageBar: 90
 
 
-        },
+    }, {
+        questionNumber: 10,
+        question: "Which of the following is a characteristic of the SaaS applications?",
+        choice1: "SaaS applications are reliable",
+        choice2: "SaaS applications are not customizable",
+        choice3: "SaaS applications are customizable",
+        choice4: "Non-reliable",
+        answer: 3,
+        percentageBar: 10
 
-    ]
-    questionTotal.innerHTML = quizQuestions.length;
 
-    //Callback Listeners
-    homeButton.addEventListener('click', homeBtnClick);
-    submitBtn.addEventListener('click', submitBtnClick);
+    },
+
+]
+questionTotal.innerHTML = quizQuestions.length;
 
 
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestion = [...quizQuestions];
+    console.log(availableQuestion);
+    getNewQuestion();
 }
-
-function homeBtnClick() {
-    homepageSection.style.display = 'block';
-    contentSection.style.display = 'none'
-}
-
-function submitBtnClick() {
-
-    if (nameInput.value == null) {
-        errmsg.style.display = 'block';
-
-
-    } else {
-        homepageSection.style.display = 'none';
-
+getNewQuestion = () => {
+    if (availableQuestion.length === 0 || questionCounter > 10) {
+        return window.location.assign("/index.html")
     }
 
+    questionCounter++;
+    currentQuestion.innerHTML = questionCounter
+    const questionIndex = Math.floor(Math.random());
+    console.log(questionIndex)
+    currentQuestion = availableQuestion[questionIndex];
+    console.log(currentQuestion)
+    question.innerText = currentQuestion.question;
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion["choice" + number];
+        availableQuestion.splice(questionIndex, 1);
+        acceptingAnswer = true;
+    })
 
 }
 
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if (!acceptingAnswer) return;
+        acceptingAnswer = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        getNewQuestion();
+        console.log(selectedAnswer)
+    })
+})
+startGame();
